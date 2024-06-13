5d7f0ca0f887845c9e6319419ba27fbfbc3de59f
kee236 <31869356+kee236@users.noreply.github.com>
2024-06-13 22:02:44 +0700

    Update app

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+{
+    "facebook": {
+        "verify_token": "YOUR_VERIFY_TOKEN",
+        "access_token": "YOUR_PAGE_ACCESS_TOKEN"
+    },
+    "dialogflow": {
+        "project_id": "YOUR_DIALOGFLOW_PROJECT_ID"
+    },
+    "openai": {
+        "api_key": "YOUR_OPENAI_API_KEY"
+    }
+}
